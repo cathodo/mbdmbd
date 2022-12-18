dac271c7c8a9a38003276f9f3df680fa4f3ab83a
Liam R Mitchell <liam.mitchell@uleth.ca>
2022-12-18 00:50:44 +0000

    R python integration setup for plotting stuff. added scaling params to the kmer table so I can figure out what they are for

--- mbdmbd.py
+++ mbdmbd.py
@@ -40,16 +40,44 @@ def extract_bam_data(r, f5dir):
     read = str(r).split('\t')
     # for getting f5 raw sig
     read_name = "read_" + read[0]
-    # move info and fields added to bam via guppy 6.3
+    # for reporting sequence
+    seq = read[9]
+    # move info and fields added to bam via guppy 6.4
     guppy_data = ast.literal_eval(ast.literal_eval(read[11].__repr__().replace('array', '')))
-    # indices of signal array which correspond to read (number of 1's = read len)
-    move_table = guppy_data[0][1][1]
-    # length of each signal segment per base
-    stride_len = move_table.pop(0)
-    # number of signal ind to start at
-    trimmed_samp = guppy_data[8][1]
-    # filename for the f5 which corresponds to this (glob should never resolve > 1 file)
-    f5_fn = glob.glob(os.path.join(f5dir, "fast5_*", guppy_data[6][1]))[0]
+    ######################################################
+    ############# PARSE the added fields #################
+    ######################################################
+    for g in guppy_data:
+        code = g[0]
+        value = g[1]
+        # see https://github.com/nanoporetech/bonito/blob/master/documentation/SAM.md#read-tags
+        if code == "mv":
+            # indices of signal array which correspond to read (number of 1's = read len)
+            move_table = value[1]
+            # length of each signal segment per base
+            stride_len = move_table.pop(0)
+        elif code == "ts":
+            # number of signal ind to start at
+            trimmed_samp = value
+        elif code == "f5":
+            # filename for the f5 which corresponds to this (glob should never resolve > 1 file)
+            f5_fn = glob.glob(os.path.join(f5dir, "fast5_*", value))[0]
+        elif code == "sv":
+            # make sure it's the scaling we expect
+            if value != "med_mad":
+                print("Warning: unexpected scaling version, data scaling may not make sense")
+        elif code == "sm":
+            # scaling midpoint
+            scale_mid = value
+        elif code == "sd":
+            scale_dis = value
+        elif code == "st":
+            start_time = value
+        else:
+            continue
+    #####################################################
+    ########### reshape data into sig + seq #############
+    #####################################################
     f = h5py.File(f5_fn, 'r')
     full_sig = extract_f5_rawseq(f, read_name)
     # start and end for the fulllength seq
@@ -61,10 +89,10 @@ def extract_bam_data(r, f5dir):
     # get indices of move table
     ss_ind = np.where([b==1 for b in move_table])[0]
     sig = stride_sig[ss_ind]
-    ## return seq, sig
-    return read[9], sig
+    # output
+    return seq, sig, scale_mid, scale_dis, start_time
 
-def make_kmer_table_from_bamfile(bamfile, f5dir, klen):
+def make_kmer_table_from_bamfile(bamfile, f5dir, targetBase, klen, verbose):
     #########################
     ####### iter bams #######
     #########################
@@ -75,14 +103,17 @@ def make_kmer_table_from_bamfile(bamfile, f5dir, klen):
     big_list = []
     ## this verbosity thing is to try and avoid the missing idx msg 
     ## solution according to https://github.com/pysam-developers/pysam/issues/939#issuecomment-669016051
-    save = pysam.set_verbosity(0)
+    if !verbose:
+        save = pysam.set_verbosity(0)
     bam = pysam.AlignmentFile(bamfile, 'rb', check_sq=False)
-    pysam.set_verbosity(save)
+    if !verbose:
+        pysam.set_verbosity(save)
     for r in bam.fetch(until_eof=True):
-        ## just A for now 
-        seq_all, sig_all = extract_bam_data(r, f5dir)
+        ## get bam data
+        seq_all, sig_all, sm, sd, st = extract_bam_data(r, f5dir)
+        ## handle bam data
         rn = "read_" + str(r).split('\t')[0]
-        seq_idx = [args.targetBase == b for b in seq_all]
+        seq_idx = [targetBase == b for b in seq_all]
         iw = np.where(seq_idx)[0]
         ## vector of individual kmer seq/sig pairs
         seq = [seq_all[i-negshift:i+posshift] for i in iw]
@@ -91,21 +122,38 @@ def make_kmer_table_from_bamfile(bamfile, f5dir, klen):
         iw2 = np.where([len(s) == klen for s in seq])[0]
         # check
         if len(seq) == 0 | len(sig) == 0:
-            print("ACHTUNG, ME VECTOR IS EMPTY")
+            if verbose:
+                print("ACHTUNG, ME VECTOR IS EMPTY")
             continue
         if len(seq) != len(sig):
-            print("AVAST AND CURSES, ME VECTORS' LENGTHS ARE ASKEW")
+            if verbose:
+                print("AVAST AND CURSES, ME VECTORS' LENGTHS ARE ASKEW")
             continue
         # make serieses
-        big_list.append(pd.DataFrame([pd.Series(data={'signal':sig[i].flatten(),'kmer':seq[i],'read_name':rn,'seqloc':str(iw[i])}) for i in iw2]))
+        big_list.append(pd.DataFrame([pd.Series(data={
+            'signal':sig[i].flatten(),
+            'kmer':seq[i],
+            'read_name':rn,
+            'seqloc':str(iw[i]),
+            'scale_mid':sm,
+            'scale_dis':sd,
+            'start_time':st
+            }) for i in iw2]))
     ##############################
     kmer_table = pd.concat(big_list).reset_index()
     del big_list, kmer_table['index']
     return kmer_table
 
-def build_kmer_table(bamdir, f5dir, klen):
-    all_bam_files = glob.glob(os.path.join(bamdir, "pass/*.bam"))
-    kmer_table_list = joblib.Parallel(n_jobs=50, verbose=10)(joblib.delayed(make_kmer_table_from_bamfile)(file_name,f5dir,klen) for file_name in all_bam_files)
+def build_kmer_table(args):
+    all_bam_files = glob.glob(os.path.join(args.bamdir, "pass/*.bam"))
+    kmer_table_list = joblib.Parallel(n_jobs=50, verbose=10)(
+        joblib.delayed(make_kmer_table_from_bamfile)
+            (file_name,
+            args.f5dir,
+            args.targetBase,
+            args.ctlen,
+            args.verbose) for file_name in all_bam_files
+    )
     kmer_table = pd.concat(kmer_table_list).reset_index()
     return kmer_table
 
@@ -116,11 +164,11 @@ def get_kmer_table(argv=sys.argv):
     # set bars verbosity
     tqdm.pandas(disable = not args.verbose)
     ### load data
-    kmer_table = build_kmer_table(args.bamdir, args.f5dir, args.ctlen)
+    kmer_table = build_kmer_table(args)
     print("it's been {} days since you looked at me".format((time.time()-st)/60/60/24))
     # NOTE to get raw signal do this:
     # X = np.array([x for x in kmer_table['signal']])
     return kmer_table
 
 if __name__=="__main__":
-    get_kmer_table(sys.argv)
+    kmer_table = get_kmer_table(sys.argv)
